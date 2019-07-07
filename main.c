fe2c68564f185dcb16b3c86e7f5979bc44c5c67f
baiqiang.hong <baiqiang.hong@stromasys.com>
2019-07-07 23:40:35 +0800

    add errno.

--- main.c
+++ main.c
@@ -18,14 +18,17 @@
 #include <arpa/inet.h>
 #include <time.h>
 #include <unistd.h>
+#include <errno.h>
+#include <string.h>
+
 #define MAXLINE 1024
 /*
  *
  */
 int main(int argc, char** argv) {
 
-    int sockfd;
-    int ret;
+    int sockfd = 0;
+    int ret = 0;
     char recvline[MAXLINE+1];
     struct sockaddr_in servaddr;
 
@@ -43,10 +46,10 @@ int main(int argc, char** argv) {
     }
 
     if (connect(sockfd, (struct sockaddr *)&servaddr, sizeof(servaddr)) < 0) {
-        printf("connect error");
+        printf("connect error: %d: %s\n", errno, strerror(errno));
         return 1;
     }
-    while ((ret = read(sockfd, recvline, sizeof(recvline)-1)) > 0) {
+    while ((ret = read(sockfd, recvline, 50)) > 0) {
         recvline[ret] = 0;
         if (fputs(recvline, stdout) == EOF) {
             printf("fputs error");

a8dc283fc316fd7ffe6501b6fd6854eefdfd2558
baiqiang.hong <baiqiang.hong@stromasys.com>
2019-07-07 19:01:28 +0800

    client return with 1 once the error happens

--- main.c
+++ main.c
@@ -18,7 +18,7 @@
 #include <arpa/inet.h>
 #include <time.h>
 #include <unistd.h>
-#define MAXLINE 30
+#define MAXLINE 1024
 /*
  *
  */
@@ -29,28 +29,35 @@ int main(int argc, char** argv) {
     char recvline[MAXLINE+1];
     struct sockaddr_in servaddr;
 
-    if ((sockfd = socket(AF_INET, SOCK_STREAM, 0)) < 0)
+    if ((sockfd = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
         printf("socket error");
-
+        return 1;
+    }
     bzero(&servaddr, sizeof(servaddr));
     servaddr.sin_family = AF_INET;
     servaddr.sin_port = htons(9090);
 
-    if (inet_pton(AF_INET, argv[1], &servaddr.sin_addr) <= 0)
+    if (inet_pton(AF_INET, argv[1], &servaddr.sin_addr) <= 0) {
         printf("inet_pton error");
-
-    if (connect(sockfd, (struct sockaddr *)&servaddr, sizeof(servaddr)) < 0)
-        printf("connect error");
-
-    while ((ret = read(sockfd, recvline, MAXLINE)) > 0) {
-        recvline[ret] == 0;
-        if (fputs(recvline, stdout) == EOF)
-            printf("fputs error");
+        return 1;
     }
 
-    if (ret < 0)
-        printf("read error");
+    if (connect(sockfd, (struct sockaddr *)&servaddr, sizeof(servaddr)) < 0) {
+        printf("connect error");
+        return 1;
+    }
+    while ((ret = read(sockfd, recvline, sizeof(recvline)-1)) > 0) {
+        recvline[ret] = 0;
+        if (fputs(recvline, stdout) == EOF) {
+            printf("fputs error");
+            return 1;
+        }
+    }
 
+    if (ret < 0) {
+        printf("read error");
+        return 1;
+    }
     return (EXIT_SUCCESS);
 }
 
